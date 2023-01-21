fbda68e8a6bb69187df5ca3b0b677fc454b76691
Pavel Kosarev <pavel999kosarev@gmail.com>
2023-01-21 15:32:31 +0300

    add news form JSON at news page

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,4 @@
+import json
 from datetime import datetime
 
 from django.views.generic import TemplateView
@@ -11,6 +12,10 @@ class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
 
     def get_context_data(self, **kwargs):
+        # open json file with news
+        with open("news.json", encoding="utf-8") as f:
+            data = json.load(f)
+
         # Get all previous data
         context = super().get_context_data(**kwargs)
         # Create your own data
@@ -18,6 +23,8 @@ class NewsPageView(TemplateView):
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
         context["range"] = range(5)
         context["datetime_obj"] = datetime.now()
+        # add news at context
+        context["data"] = data
         return context
 
 
